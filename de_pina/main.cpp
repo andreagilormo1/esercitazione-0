13be8e79579a90a9e42136c917dfe3cb870da60b
Andrea Gilormo <andreagilormo17@gmail.com>
2026-05-27 20:17:40 +0200

    consegna_de_pina

--- de_pina/main.cpp
+++ de_pina/main.cpp
@@ -5,6 +5,9 @@
 #include <string>
 #include <vector>
 #include <map>
+#include "Eigen/Dense"
+#include "Eigen/SVD"
+
 
 
 using namespace std;
@@ -49,6 +52,50 @@ void ricevi_resistori(const undirected_graph& grafo, const map<unidirected_edge,
     }
 }
 
+void riempi_R(Eigen::MatrixXd& R, const set<unidirected_edge>& resistori, const map<unidirected_edge, double>& peso_arco) {
+    int i = 0;
+    for (const auto& arco : resistori) {
+        R(i, i) = peso_arco.at(arco);
+        i++;
+    }
+}
+
+void riempi_B(Eigen::MatrixXd& B, const set<unidirected_edge>& resistori, map<int, vector<int>> registro_cicli) {
+    
+    for (size_t i = 0; i < registro_cicli.size(); i++) {
+        const auto& ciclo = registro_cicli[i];
+        
+        
+        for (size_t nodo = 0; nodo < ciclo.size() - 1; nodo++) {
+            int u = ciclo[nodo];     
+            int v = ciclo[nodo + 1]; 
+
+            
+            int riga_resistore = 0;
+            
+            for (const auto& arco : resistori) {
+               
+                if ((arco.from() == u && arco.to() == v) || (arco.from() == v && arco.to() == u)) {
+                    
+                   
+                    if (u < v) {
+                        B(riga_resistore, i) = 1.0;
+                    } 
+                   
+                    else {
+                        B(riga_resistore, i) = -1.0;
+                    }
+                    
+                    
+                    break; 
+                }
+                
+                riga_resistore++;
+            }
+        }
+    }
+}
+
 
 
           
@@ -88,6 +135,22 @@ int main() {
 
     set<unidirected_edge> resistori;
     ricevi_resistori(circuito, tipo_arco, resistori);
+	
+	Eigen::MatrixXd R(resistori.size(), resistori.size());
+    R = Eigen::MatrixXd::Zero(resistori.size(), resistori.size());
+    riempi_R(R, resistori, peso_arco);
+
+    Eigen::MatrixXd B(resistori.size(), (circuito.archi.size()-circuito.nodi.size()+1));
+    B = Eigen::MatrixXd::Zero(resistori.size(), (circuito.archi.size()-circuito.nodi.size()+1));
+    riempi_B(B, resistori, registro_cicli);
+
+    cout<<B<<endl;
+	cout<<endl;
+	cout<<endl;
+	cout<<R<<endl;
+
+	
+	
 
    
 
